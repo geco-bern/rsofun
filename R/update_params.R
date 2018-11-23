a622f677dbd445300844a8558882d8280c868baa
stineb <b.stocker@imperial.ac.uk>
2018-11-23 16:59:08 +0100

    bugfix in update_params

--- R/update_params.R
+++ R/update_params.R
@@ -9,47 +9,41 @@
 #' @export
 #'
 update_params <- function( params_opt, dir_sofun ){
+  
   system( paste0( "cp ", dir_sofun, "params_std/* ", dir_sofun, "params/") )
+
+  filn <- paste0( dir_sofun, "params/params_gpp_pmodel.dat") 
+  content <- readLines( filn )
   
   if ("kphio" %in% names(params_opt)){
-    filn <- paste0( dir_sofun, "params/params_gpp_pmodel.dat") 
-    content <- readLines( filn )
     linenr <- which( grepl("kphio", content ) )
     len <- str_length(content[linenr])
     paramvals <- str_sub( content[linenr], start = 41, end = len )
     content[linenr] <- str_replace( content[linenr], paramvals, format( params_opt$kphio, digits = 8 ) )
-    writeLines( content, con = filn )
   }
 
   if ("temp_ramp_edge" %in% names(params_opt)){
-    filn <- paste0( dir_sofun, "params/params_gpp_pmodel.dat") 
-    content <- readLines( filn )
     linenr <- which( grepl("temp_ramp_edge", content ) )
     len <- str_length(content[linenr])
     paramvals <- str_sub( content[linenr], start = 41, end = len )
     content[linenr] <- str_replace( content[linenr], paramvals, format( params_opt$temp_ramp_edge, digits = 8 ) )
-    writeLines( content, con = filn )
   }
 
   if ("soilm_par_a" %in% names(params_opt)){
-    filn <- paste0( dir_sofun, "params/params_gpp_pmodel.dat") 
-    content <- readLines( filn )
     linenr <- which( grepl("soilm_par_a", content ) )
     len <- str_length(content[linenr])
     paramvals <- str_sub( content[linenr], start = 41, end = len )
     content[linenr] <- str_replace( content[linenr], paramvals, format( params_opt$soilm_par_a, digits = 8 ) )
-    writeLines( content, con = filn )
   }
 
   if ("soilm_par_b" %in% names(params_opt)){
-    filn <- paste0( dir_sofun, "params/params_gpp_pmodel.dat") 
-    content <- readLines( filn )
     linenr <- which( grepl("soilm_par_b", content ) )
     len <- str_length(content[linenr])
     paramvals <- str_sub( content[linenr], start = 41, end = len )
     content[linenr] <- str_replace( content[linenr], paramvals, format( params_opt$soilm_par_b, digits = 8 ) )
-    writeLines( content, con = filn )
   }
-
+  
+  writeLines( content, con = filn )
+  
   return(paramvals)
 }
