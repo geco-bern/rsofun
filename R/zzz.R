d21b815ad6490defece7df37dfbb8579758bccd2
stineb <benjamin.stocker@gmail.com>
2019-01-09 00:20:32 +0100

    added file

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+# Adopted from http://r-pkgs.had.co.nz/r.html
+.onLoad <- function( libname, pkgname ){
+  op <- options()
+  op.rsofun <- list(
+    rsofun.name = "Benjamin Stocker",
+    rsofun.desc.author = "Benjamin Stocker <benjamin.stocker@gmail.com> [aut, cre]",
+    rsofun.desc.license = "GNU General Public License",
+    rsofun.desc = list(),
+    rsofun.dir.sofun = ""
+  )
+  toset <- !(names(op.rsofun) %in% names(op))
+  if(any(toset)) options(op.rsofun[toset])
+
+  invisible()
+}
