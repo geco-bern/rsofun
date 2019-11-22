8560682ad4b18b9b5894a11f5fcd87209e8ee291
Benjamin Stocker <benjamin.stocker@gmail.com>
2019-11-22 10:03:11 +0100

    changed subdirectory for oob leftoutsite outputs

--- R/oob_calib_eval_sofun.R
+++ R/oob_calib_eval_sofun.R
@@ -66,7 +66,11 @@ oob_calib_eval_sofun_bysite <- function(evalsite, setup, settings_calib, setting
   
   print(paste("oob_calib_eval_sofun_bysite() for site", evalsite))
   
-  outfil <- paste0(settings_calib$dir_results, "/out_eval_leftout_", evalsite, ".Rdata")
+  dirn <- paste0( settings_calib$dir_results, "/oob_", settings_calib$name)
+  if (!dir.exists(dirn)){
+    system(paste0("mkdir -p ", dirn))
+  }
+  outfil <- paste0(dirn, "/out_eval_leftout_", evalsite, ".Rdata")
   
   if (file.exists(outfil)){
 
@@ -121,7 +125,7 @@ oob_calib_eval_sofun_bysite <- function(evalsite, setup, settings_calib, setting
     ##------------------------------------------------
     ## Update parameters and run at evaluation site
     ##------------------------------------------------
-    filn <- paste0( settings_calib$dir_results, "/params_opt_", settings_calib$name, ".csv")
+    filn <- paste0( dirn, "/params_opt_", settings_calib$name, ".csv")
     params_opt <- readr::read_csv( filn )
     nothing <- update_params( params_opt, settings_sims$dir_sofun )
     
