30ca70fcbfbc9baf99369509328699a26451d515
Benjamin Stocker <benjamin.stocker@gmail.com>
2020-05-18 17:16:49 +0200

    reduced kphio for lm3ppa-pmodel setup

--- src/gpp_lm3ppa.mod.f90
+++ src/gpp_lm3ppa.mod.f90
@@ -26,7 +26,7 @@ module md_gpp_lm3ppa
 
   ! PFT-DEPENDENT PARAMETERS
   type pftparamstype_gpp
-    real :: kphio = 0.24    ! hard-coded here, is a calibratable parameter in P-model, unrealistically high here to match ballpark of original model
+    real :: kphio = 0.08    ! hard-coded here, is a calibratable parameter in P-model, unrealistically high here to match ballpark of original model
   end type pftparamstype_gpp
 
   type(pftparamstype_gpp) :: params_pft_gpp
