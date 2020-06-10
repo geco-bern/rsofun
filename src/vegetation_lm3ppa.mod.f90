eed7b5b3e850b0347820e00d6530d62f839d1bf9
Laura Marqués <lmarques@gl142-110.ethz.ch>
2020-06-10 10:33:40 +0200

    Added formulations to example_lm3ppa

--- src/vegetation_lm3ppa.mod.f90
+++ src/vegetation_lm3ppa.mod.f90
@@ -668,61 +668,61 @@ contains
    real, parameter :: min_nindivs = 1e-5 ! 2e-15 ! 1/m. If nindivs is less than this number, 
    ! then the entire cohort is killed; 2e-15 is approximately 1 individual per Earth 
 
-   if ((trim(myinterface%params_siml%method_mortality) == "const_selfthin")) then
+   ! if ((trim(myinterface%params_siml%method_mortality) == "const_selfthin")) then
 
-    ! check if current CAI is greater than maximum CAI
-    if (vegn%CAI > CAI_max) then
+   !  ! check if current CAI is greater than maximum CAI
+   !  if (vegn%CAI > CAI_max) then
 
-      ! relayer cohorts
-      ! cohorts must be ordered by height after this, whith cohort(1) being the shortest
-      call relayer_cohorts(...)
+   !    ! relayer cohorts
+   !    ! cohorts must be ordered by height after this, whith cohort(1) being the shortest
+   !    call relayer_cohorts(...)
 
-      ! Get "partial" CAI of all cohorts shorter/equal than the current cohort
-      cai_partial(:) = 0.0 
-      do i = 1, vegn%n_cohorts
-        cc => vegn%cohorts(i)
-        if (i>1) then
-          cai_partial(i) = cai_partial(1:(i-1)) + cc%crownarea * cc%nindivs
-        else
-          cai_partial(i) = cc%crownarea * cc%nindivs
-        end if
-      end do
+   !    ! Get "partial" CAI of all cohorts shorter/equal than the current cohort
+   !    cai_partial(:) = 0.0 
+   !    do i = 1, vegn%n_cohorts
+   !      cc => vegn%cohorts(i)
+   !      if (i>1) then
+   !        cai_partial(i) = cai_partial(1:(i-1)) + cc%crownarea * cc%nindivs
+   !      else
+   !        cai_partial(i) = cc%crownarea * cc%nindivs
+   !      end if
+   !    end do
 
-      ! determine the cohort where cai_partial exceeds critical value
-      i_crit = 1
-      do while (cai_partial(i_crit) < CAI_max)
-        i_crit = i_crit + 1
-      end do
-      deathrate = CAI_max / cai_partial(i_crit)
+   !    ! determine the cohort where cai_partial exceeds critical value
+   !    i_crit = 1
+   !    do while (cai_partial(i_crit) < CAI_max)
+   !      i_crit = i_crit + 1
+   !    end do
+   !    deathrate = CAI_max / cai_partial(i_crit)
 
-      cc => vegn%cohorts(i_crit)
+   !    cc => vegn%cohorts(i_crit)
 
-      deadtrees = cc%nindivs * deathrate
+   !    deadtrees = cc%nindivs * deathrate
 
-      ! Carbon and Nitrogen from dead plants to soil pools
-      call plant2soil(vegn,cc,deadtrees)
+   !    ! Carbon and Nitrogen from dead plants to soil pools
+   !    call plant2soil(vegn,cc,deadtrees)
 
-      ! Update plant density
-      cc%nindivs = cc%nindivs - deadtrees
+   !    ! Update plant density
+   !    cc%nindivs = cc%nindivs - deadtrees
 
-      ! just to be safe...
-      if (i_crit < vegn%n_cohorts) then
-        do i = (i_crit+1), vegn%n_cohorts
-          deathrate = 1.0
-          cc => vegn%cohorts(i)
+   !    ! just to be safe...
+   !    if (i_crit < vegn%n_cohorts) then
+   !      do i = (i_crit+1), vegn%n_cohorts
+   !        deathrate = 1.0
+   !        cc => vegn%cohorts(i)
 
-          deadtrees = cc%nindivs * deathrate
+   !        deadtrees = cc%nindivs * deathrate
 
-          ! Carbon and Nitrogen from dead plants to soil pools
-          call plant2soil(vegn,cc,deadtrees)
+   !        ! Carbon and Nitrogen from dead plants to soil pools
+   !        call plant2soil(vegn,cc,deadtrees)
 
-          ! Update plant density
-          cc%nindivs = cc%nindivs - deadtrees
+   !        ! Update plant density
+   !        cc%nindivs = cc%nindivs - deadtrees
 
-        end do
-      end if
+   !      end do
+   !    end if
 
-   else
+   ! else
 
 
    do i = 1, vegn%n_cohorts
