49b08d3f639819cf3d166821ff9ab8f07fecbc5e
Laura Marques <lauramarqueslopez@gmail.com>
2021-07-01 16:51:02 +0200

    Prep running calib in Euler

--- vignettes_add/euler_runs.R
+++ vignettes_add/euler_runs.R
@@ -3,11 +3,12 @@ library(tibble)
 if(!require(devtools)){install.packages(devtools)}
 devtools::install_github("stineb/rsofun")
 library(rsofun)
+install()
 library(ggplot2)
 library(ingestr)
 library(multidplyr)
 
-load("/input_data/df_drivers_DBH_gs.RData")
+load("/cluster/home/lmarques/rsofun/input_data/df_drivers_DBH_gs.RData")
 
 start <- Sys.time()
 df_output <- runread_lm3ppa_f(
@@ -17,9 +18,9 @@ df_output <- runread_lm3ppa_f(
 )
 print(Sys.time() - start)
 
-write.csv(df_calib_DBH_gs$data[[1]]$output_annual_tile, "/output_euler/ea1sa1DBHgl_out_annual_tileEULER.csv")
+write.csv(df_calib_DBH_gs$data[[1]]$output_annual_tile, "/cluster/home/lmarques/rsofun/output_euler/ea1sa1DBHgl_out_annual_tileEULER.csv")
 
-load("/input_data/ddf_obs.RData")
+load("/cluster/home/lmarques/rsofun/input_data/ddf_obs.RData")
 
 settings_calib_DBH_gs <- list(
   method              = "gensa",
@@ -33,7 +34,8 @@ settings_calib_DBH_gs <- list(
   par                 = list(phiRL = list(lower=0.5, upper=5, init=3.5),
                              LAI_light = list(lower=2, upper=5, init=3.5),
                              tf_base = list(lower=0.5, upper=1.5, init=1),
-                             par_mort = list(lower=0.1, upper=2, init=1))
+                             par_mort = list(lower=0.1, upper=2, init=1),
+                             par_mort_under = list(lower=0.1, upper=2, init=1))
 )
 
 set.seed(1152)
@@ -43,4 +45,4 @@ settings_calib_DBH_gs <- calib_sofun(
   settings = settings_calib_DBH_gs
 )
 
-save(settings_calib_DBH_gs, file = "/input_data/settings_calib_DBH_gsEULER.RData")
+save(settings_calib_DBH_gs, file = "/cluster/home/lmarques/rsofun/input_data/settings_calib_DBH_gsEULER.RData")
