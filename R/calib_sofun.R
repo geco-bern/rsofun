3001f7176c5ccd593d1a8577196036007c1bd404
Laura Marques <lauramarqueslopez@gmail.com>
2021-07-05 09:37:48 +0200

    Corrected sizedist vector

--- R/calib_sofun.R
+++ R/calib_sofun.R
@@ -446,7 +446,7 @@ cost_rmse_lm3ppa_gsleuning <- function( par, ddf_obs, df_drivers, inverse = FALS
     dplyr::summarise(GPP = mean(GPP), LAI= quantile(LAI, probs = 0.95, na.rm=T), Biomass=mean(plantC))
 
   # Add size dsitribution from observations
-  size_bins <- c(12.1,17.8,28.0,40.8,54.0,105.4)
+  sizedist <- c(12.1,17.8,28.0,40.8,54.0,105.4)
 
   df_mod_sizedist <- df$data[[1]]$output_annual_cohorts %>%
     dplyr::filter(year>df_drivers$params_siml[[1]]$spinupyears) %>% 
