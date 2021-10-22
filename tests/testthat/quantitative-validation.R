9ca7219d403433966680cf41937d6620b1d1791d
khufkens <koen.hufkens@gmail.com>
2021-10-22 13:16:24 +0200

    remove plot

--- tests/testthat/quantitative-validation.R
+++ tests/testthat/quantitative-validation.R
@@ -3,8 +3,12 @@ context("Test model output (values)")
 test_that("p-model quantitative check", {
   skip_on_cran()
   
+  # grab gpp data from the validation set
+  # for FR-Pue
   gpp <- p_model_validation$data[[1]]$gpp
   
+  # set model drivers to the NPHT paper
+  # ones
   params_modl <- list(
     kphio           = 0.09423773,
     soilm_par_a     = 0.33349283,
@@ -18,9 +22,6 @@ test_that("p-model quantitative check", {
     rsofun::p_model_drivers,
     par = params_modl
   )$data[[1]]$gpp
-  
-  plot(output$gpp)
-  lines(gpp)
   
   # normal tolerance ~ 0.67
   tolerance <- mean(abs(output - gpp), na.rm = TRUE)/
