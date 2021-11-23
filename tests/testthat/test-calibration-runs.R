d0d191209ef1e79c90cd6e3af2ec07d09a1fb803
Koen Hufkens <koen.hufkens@gmail.com>
2021-11-23 20:23:52 +0100

    test

--- tests/testthat/test-calibration-runs.R
+++ tests/testthat/test-calibration-runs.R
@@ -1,72 +1,72 @@
 context("test calibration framework and its parameters")
 
-# test_that("test calibration routine p-model", {
-#   skip_on_cran()
-#   
-#   drivers <- p_model_drivers
-#   obs <- p_model_validation
-#   
-#   settings <- list(
-#     method              = "bayesiantools",
-#     targetvars          = c("gpp"),
-#     timescale           = list(targets_obs = "y"),
-#     sitenames           = "FR-Pue",
-#     metric              = cost_rmse_kphio,
-#     dir_results         = "./",
-#     name                = "ORG",
-#     control = list(
-#       sampler = "DEzs",
-#       settings = list(
-#         burnin = 10,
-#         iterations = 50
-#       )
-#     ),
-#     par = list(
-#       kphio = list(lower=0.04, upper=0.1, init = 0.05),
-#       a = list(lower=0, upper=5, init = 3.5),
-#       b = list(lower=1, upper=5, init=3.5),
-#       tau = list(lower=0, upper=15, init=1),
-#       shape = list(lower=0, upper=0.1, init=0)
-#       )
-#   )
-#   
-#   pars <- calib_sofun(
-#     drivers = drivers,  
-#     obs = obs,
-#     settings = settings
-#   )
-#   
-#   # test for correctly returned values
-#   expect_type(pars, "list")
-#   
-#   settings <- list(
-#     method              = "gensa",
-#     targetvars          = c("gpp"),
-#     timescale           = list(targets_obs = "y"),
-#     maxit               = 10,
-#     sitenames           = "FR-Pue",
-#     metric              = cost_rmse_kphio,
-#     dir_results         = "./",
-#     name                = "ORG",
-#     control = list(
-#       max.call = 10
-#     ),
-#     par = list(kphio = list(lower=0.04, upper=0.09, init=0.05),
-#                phiRL = list(lower=0.5, upper=5, init=3.5),
-#                LAI_light = list(lower=2, upper=5, init=3.5),
-#                tf_base = list(lower=0.5, upper=1.5, init=1),
-#                par_mort = list(lower=0.1, upper=2, init=1))
-#   )
-#   
-#   pars <- calib_sofun(
-#     drivers = drivers,  
-#     obs = obs,
-#     settings = settings
-#   )
-#   
-#   # test for correctly returned values
-#   expect_type(pars, "list")
-# })
+test_that("test calibration routine p-model", {
+  skip_on_cran()
+
+  drivers <- p_model_drivers
+  obs <- p_model_validation
+
+  settings <- list(
+    method              = "bayesiantools",
+    targetvars          = c("gpp"),
+    timescale           = list(targets_obs = "y"),
+    sitenames           = "FR-Pue",
+    metric              = cost_rmse_kphio,
+    dir_results         = "./",
+    name                = "ORG",
+    control = list(
+      sampler = "DEzs",
+      settings = list(
+        burnin = 10,
+        iterations = 50
+      )
+    ),
+    par = list(
+      kphio = list(lower=0.04, upper=0.1, init = 0.05),
+      a = list(lower=0, upper=5, init = 3.5),
+      b = list(lower=1, upper=5, init=3.5),
+      tau = list(lower=0, upper=15, init=1),
+      shape = list(lower=0, upper=0.1, init=0)
+      )
+  )
+
+  pars <- calib_sofun(
+    drivers = drivers,
+    obs = obs,
+    settings = settings
+  )
+
+  # test for correctly returned values
+  expect_type(pars, "list")
+
+  settings <- list(
+    method              = "gensa",
+    targetvars          = c("gpp"),
+    timescale           = list(targets_obs = "y"),
+    maxit               = 10,
+    sitenames           = "FR-Pue",
+    metric              = cost_rmse_kphio,
+    dir_results         = "./",
+    name                = "ORG",
+    control = list(
+      max.call = 10
+    ),
+    par = list(kphio = list(lower=0.04, upper=0.09, init=0.05),
+               phiRL = list(lower=0.5, upper=5, init=3.5),
+               LAI_light = list(lower=2, upper=5, init=3.5),
+               tf_base = list(lower=0.5, upper=1.5, init=1),
+               par_mort = list(lower=0.1, upper=2, init=1))
+  )
+
+  pars <- calib_sofun(
+    drivers = drivers,
+    obs = obs,
+    settings = settings
+  )
+
+  # test for correctly returned values
+  expect_type(pars, "list")
+})
 
 
 test_that("test calibration routine lm3ppa", {
@@ -91,11 +91,11 @@ test_that("test calibration routine lm3ppa", {
       sampler = "DEzs",
       settings = list(
         burnin = 1,
-        iterations = 5
+        iterations = 4
       )
     ),
     par = list(
-         phiRL = list(lower=0.5, upper=5, init=3.5),
+        phiRL = list(lower=0.5, upper=5, init=3.5),
         LAI_light = list(lower=2, upper=5, init=3.5),
         tf_base = list(lower=0.5, upper=1.5, init=1),
         par_mort = list(lower=0.1, upper=2, init=1))
