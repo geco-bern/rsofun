cfb66a84ab4e42a0f799f3958225e0d381c44a78
khufkens <koen.hufkens@gmail.com>
2022-03-16 17:26:33 +0100

    addressing NOTE in checks, no solution for segfault yet

--- tests/testthat/test-calibration-lm3ppa.R
+++ tests/testthat/test-calibration-lm3ppa.R
@@ -1,55 +1,15 @@
 context("test calibration framework and its parameters")
 
-test_that("test calibration routine p-model (BT)", {
-  skip_on_cran()
-
-  drivers <- p_model_drivers
-  obs <- p_model_validation
-  
-  settings <- list(
-    method              = "bayesiantools",
-    targets             = c("gpp"),
-    sitenames           = "FR-Pue",
-    metric              = likelihood_pmodel,
-    control = list(
-      sampler = "DEzs",
-      settings = list(
-        burnin = 1,
-        iterations = 4
-        )
-    ),
-    par = list(
-      kphio = list(lower=0.04, upper=0.09, init=0.05),
-      phiRL = list(lower=0.5, upper=5, init=3.5),
-      LAI_light = list(lower=2, upper=5, init=3.5),
-      tf_base = list(lower=0.5, upper=1.5, init=1),
-      par_mort = list(lower=0.1, upper=2, init=1),
-      # the observed data estimated errors
-      err_gpp = list(lower = 0, upper = 30, init = 15)
-    )
-  )
-  
-  pars <- calib_sofun(
-    drivers = drivers,
-    obs = obs,
-    settings = settings
-  )
-
-  # test for correctly returned values
-  expect_type(pars, "list")
-})
-
 test_that("test calibration routine lm3ppa (gensa)", {
   skip_on_cran()
 
-  df_drivers <- lm3ppa_gs_leuning_drivers
-  ddf_obs <- lm3ppa_validation_2
+  df_drivers <- rsofun::lm3ppa_gs_leuning_drivers
+  ddf_obs <- rsofun::lm3ppa_validation_2
   df_drivers$params_siml[[1]]$spinup <- FALSE
 
   # Mortality as DBH
   settings <- list(
     method              = "gensa",
-    #targets             = c("gpp"),
     timescale           = list(targets_obs = "y"),
     metric              = cost_rmse_lm3ppa_gsleuning,
     control = list(
@@ -75,15 +35,15 @@ test_that("test calibration routine lm3ppa (gensa)", {
 test_that("test calibration routine lm3ppa (Bayesiantools)", {
   skip_on_cran()
   
-  df_drivers <- lm3ppa_gs_leuning_drivers
-  ddf_obs <- lm3ppa_validation_2
+  df_drivers <- rsofun::lm3ppa_gs_leuning_drivers
+  ddf_obs <- rsofun::lm3ppa_validation_2
   df_drivers$params_siml[[1]]$spinup <- FALSE
   
   # Mortality as DBH
   settings <- list(
     method              = "bayesiantools",
     targets             = c("GPP","LAI","Density","Biomass"),
-    metric              = likelihood_lm3ppa,
+    metric              = rsofun::likelihood_lm3ppa,
     control = list(
       sampler = "DEzs",
       settings = list(
