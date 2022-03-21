69ad6ea38fdf4069736d8398e565c914ad36a486
Benjamin Stocker <benjamin.stocker@gmail.com>
2022-03-21 10:02:57 +0100

    Revert "BT"
    This reverts commit 43742e2a9ffa6e008105bcb3781d164e7a08381d.

--- R/cost_functions.R
+++ R/cost_functions.R
@@ -430,6 +430,7 @@ cost_rmse_lm3ppa_gsleuning <- function(
 #' @return the loglikelihood comparing observed and estimated values
 #' @export
 
+
 likelihood_lm3ppa <- function(
   par,
   par_names,
