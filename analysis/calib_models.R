9a512eebb80eacf3562157398ab35a169cbfd2c1
Benjamin Stocker <benjamin.stocker@gmail.com>
2022-03-29 16:55:38 +0200

    to make calib_models run here

--- analysis/calib_models.R
+++ analysis/calib_models.R
@@ -1,8 +1,8 @@
 library(rsofun)
 library(rpmodel)
 library(tidyverse)
-source("R/cost_functions.R")
-source("R/calib_sofun_2.R")
+# source("R/cost_functions.R")
+# source("R/calib_sofun_2.R")
 
 drivers <- p_model_drivers
 obs <- p_model_validation
@@ -23,16 +23,14 @@ settings <- list(
   ),
   par = list(
     kphio = list(lower=0.04, upper=0.09, init=0.05),
-    phiRL = list(lower=0.5, upper=5, init=3.5),
-    LAI_light = list(lower=2, upper=5, init=3.5),
-    tf_base = list(lower=0.5, upper=1.5, init=1),
-    par_mort = list(lower=0.1, upper=2, init=1),
+    soilm_par_a = list(lower=0.04, upper=0.09, init=0.05),
+    soilm_par_b = list(lower=0.04, upper=0.09, init=0.05),
     err_gpp = list(lower = 0, upper = 30, init = 15),
     err_gpp_unc = list(lower = 0, upper = 1, init = 1)
   )
 )
 
-pars <- calib_sofun_2(
+pars <- calib_sofun(
   drivers = drivers,
   obs = obs,
   settings = settings
