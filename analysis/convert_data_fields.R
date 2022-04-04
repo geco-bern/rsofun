0725f09ef728dcbec161a4e76dd19ee24ad8be74
khufkens <koen.hufkens@gmail.com>
2022-04-04 17:17:11 +0200

    convert data files

--- analysis/convert_data_fields.R
+++ analysis/convert_data_fields.R
@@ -3,27 +3,28 @@
 library(tidyverse)
 library(rsofun)
 
-df <- rsofun::lm3ppa_gs_leuning_drivers
-
-forcing <- df$forcing[[1]]
-forcing <- forcing %>%
+#---- gs leuning drivers ----
+lm3ppa_gs_leuning_drivers <- rsofun::lm3ppa_gs_leuning_drivers
+lm3ppa_gs_leuning_drivers$forcing[[1]] <- lm3ppa_gs_leuning_drivers$forcing[[1]] %>%
   mutate(
     date = as.Date(paste(YEAR, DOY),"%Y %j"),
+    snow = NA,
+    vpd = NA,
+    ccov_int = NA,
+    ccov = NA
+  ) %>%
+  rename(
     year = YEAR,
     doy = DOY,
     hour = HOUR,
     temp = TEMP,
     temp_soil = SoilT,
     prec = RAIN,
-    snow = NA,
-    vpd = NA,
     rh = RH,
     ppfd = Swdown,
     par = PAR,
     patm = PRESSURE,
     wind = WIND,
-    ccov_int = NA,
-    ccov = NA,
     co2 = aCO2_AW,
     swc = SWC
   ) %>%
@@ -33,5 +34,38 @@ forcing <- forcing %>%
     ccov, co2, swc
   )
 
-print(str(forcing))
+save(lm3ppa_gs_leuning_drivers, file = "data/lm3ppa_gs_leuning_drivers.rda", compress = "xz")
+rm(list=ls())
+#---- p-model drivers --- 
 
+lm3ppa_p_model_drivers <- rsofun::lm3ppa_p_model_drivers
+lm3ppa_p_model_drivers$forcing[[1]] <- lm3ppa_p_model_drivers$forcing[[1]] %>%
+  mutate(
+    date = as.Date(paste(YEAR, DOY),"%Y %j"),
+    snow = NA,
+    vpd = NA,
+    ccov_int = NA,
+    ccov = NA
+  ) %>%
+  rename(
+    year = YEAR,
+    doy = DOY,
+    hour = HOUR,
+    temp = TEMP,
+    temp_soil = SoilT,
+    prec = RAIN,
+    rh = RH,
+    ppfd = Swdown,
+    par = PAR,
+    patm = PRESSURE,
+    wind = WIND,
+    co2 = aCO2_AW,
+    swc = SWC
+  ) %>%
+  select(
+    date, year, doy, hour, temp, temp_soil,
+    prec, snow, vpd, rh, ppfd, par, patm, wind, ccov_int,
+    ccov, co2, swc
+  )
+
+save(lm3ppa_p_model_drivers, file = "data/lm3ppa_p_model_drivers.rda", compress = "xz")
