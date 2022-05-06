4815bf2c4e8ba2088ed0e19ee27a0b3dd690add1
stineb <b.stocker@imperial.ac.uk>
2022-05-06 18:14:56 +0200

    reverted to fixed allocation. should work without crash.

--- src/params_siml_pmodel.mod.f90
+++ src/params_siml_pmodel.mod.f90
@@ -133,16 +133,16 @@ contains
       endif
       out_steering%outyear = year + params_siml%firstyeartrend - params_siml%spinupyears - 1
 
-      if ( year > 3 ) then
-      ! if (year > (spinupyr_soilequil_1 + 1) ) then
-      ! if (out_steering%forcingyear > 2003 ) then
-        out_steering%dofree_alloc = .true.
-      else
-        out_steering%dofree_alloc = .false.
-      end if
+      ! if ( year > 3 ) then
+      ! ! if (year > (spinupyr_soilequil_1 + 1) ) then
+      ! ! if (out_steering%forcingyear > 2003 ) then
+      !   out_steering%dofree_alloc = .true.
+      ! else
+      !   out_steering%dofree_alloc = .false.
+      ! end if
 
-      ! ! xxx try: fixed allocation
-      ! out_steering%dofree_alloc = .false.
+      ! xxx try: fixed allocation
+      out_steering%dofree_alloc = .false.
 
       if ( (year==spinupyr_soilequil_1 .or. year==spinupyr_soilequil_2 ) .and. year<=params_siml%spinupyears) then
         out_steering%do_soilequil = .true.
