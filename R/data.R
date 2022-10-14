a36aa2ab48b7e0844eda20559de0261d2484a51c
Pepa Aran Paredes <pepa@pop-os.localdomain>
2022-10-14 19:54:19 +0200

    p_model_drivers data references

--- R/data.R
+++ R/data.R
@@ -5,24 +5,24 @@
 #'
 #' @format A tibble of driver data:
 #' \describe{
-#'   \item{sitename}{site name}
-#'   \item{forcing}{A tibble of forcing climate data, including the following variables:
+#'   \item{sitename}{a character string containing the site name}
+#'   \item{forcing}{a tibble of forcing climate data, including the following variables:
 #'     \describe{
-#'       \item{date}{}
-#'       \item{temp}{}
-#'       \item{prec}{}
-#'       \item{vpd}{}
-#'       \item{ppfd}{Photosynthetic photon flux density (PPFD) measured in ...}
-#'       \item{patm}{}
+#'       \item{date}{Date of the observation in YYYY-MM-DD format}
+#'       \item{temp}{Air temperature in $^\circ$C}
+#'       \item{prec}{Precipitation in mm d$^{-1}$ (sum of rain and snow)}
+#'       \item{vpd}{Vapour pressure deficit in Pa}
+#'       \item{ppfd}{Photosynthetic photon flux density (PPFD) in mol m$^{-2}$ d$^{-1}$}
+#'       \item{patm}{Atmospheric pressure in Pa}
 #'       \item{ccov_int}{}
-#'       \item{ccov}{}
-#'       \item{snow}{}
-#'       \item{rain}{}
+#'       \item{ccov}{Cloud coverage in %}
+#'       \item{snow}{Snow in mm d$^{-1}$}
+#'       \item{rain}{Rain in mm d$^{-1}$}
 #'       \item{fapar}{Fraction of photosynthetic active radiation (fAPAR) measured in ...}
 #'       \item{co2}{}
-#'       \item{day}{}
-#'       \item{tmin}{}
-#'       \item{tmax}{}
+#'       \item{doy}{}
+#'       \item{tmin}{Daily minimum air temperature in $^\circ$C}
+#'       \item{tmax}{Daily maximum air temperature in $^\circ$C}
 #'       }
 #'   }
 #'   \item{params_siml}{A tibble [ 1 x 18] of model parameters
@@ -74,6 +74,24 @@
 #'     }
 #'   }
 #' }
+#' 
+#' @references Pastorello, G., Trotta, C., Canfora, E. et al. 
+#' The FLUXNET2015 dataset and the ONEFlux processing pipeline for eddy covariance data. 
+#' Sci Data 7, 225 (2020). https://doi.org/10.1038/s41597-020-0534-3
+#' 
+#' University of East Anglia Climatic Research Unit; Harris, I.C.; Jones, P.D.; Osborn, T. (2021): 
+#' CRU TS4.05: Climatic Research Unit (CRU) Time-Series (TS) version 4.05 of high-resolution 
+#' gridded data of month-by-month variation in climate (Jan. 1901- Dec. 2020). 
+#' NERC EDS Centre for Environmental Data Analysis, date of citation. 
+#' https://catalogue.ceda.ac.uk/uuid/c26a65020a5e4b80b20018f148556681
+#' 
+#' Weedon, G. P., G. Balsamo, N. Bellouin,S. Gomes, M. J. Best, and P. Viterbo(2014), 
+#' The WFDEI meteorologicalforcing data set: WATCH Forcing Datamethodology applied 
+#' to ERA-Interimreanalysis data,
+#' Water Resour. Res.,50,7505–7514, doi:10.1002/2014WR015638.
+#' 
+#' Fick, S.E. and R.J. Hijmans, 2017. WorldClim 2: new 1km spatial resolution climate 
+#' surfaces for global land areas. International Journal of Climatology 37 (12): 4302-4315.
 "p_model_drivers"
 
 #' SOFUN p-model GPP validation data
