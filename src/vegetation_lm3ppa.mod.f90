372ee64218c979caf97c2e43ba27484f05840fe5
Beni Stocker <benjamin.stocker@gmail.com>
2023-02-13 19:44:33 +0100

    wip

--- src/vegetation_lm3ppa.mod.f90
+++ src/vegetation_lm3ppa.mod.f90
@@ -1782,7 +1782,7 @@ contains
       if (vegn%cohorts(i)%nindivs > mindensity) k=k+1
     enddo
 
-    ! https://github.com/computationales/rsofun/issues/24
+    ! https://github.com/geco-bern/rsofun/issues/24
     !if (k==0) then 
     !  print *, "cohort too small..."
     !  stop
