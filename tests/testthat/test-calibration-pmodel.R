39bfa4bf0c6a21d33fd55ec606038676878da5be
Pepa Aran Paredes <pepa@pop-os.localdomain>
2023-04-03 19:33:44 +0200

    subset data and liklihood cost vcmax

--- tests/testthat/test-calibration-pmodel.R
+++ tests/testthat/test-calibration-pmodel.R
@@ -87,8 +87,8 @@ test_that("test GPP calibration routine p-model (GenSA, rmse)", {
 
 test_that("test Vcmax25 calibration routine p-model (BT, rmse)", {
   skip_on_cran()
-  drivers <- p_model_drivers_vcmax25
-  obs <- rsofun::p_model_validation_vcmax25
+  drivers <- p_model_drivers_vcmax25[1:5, ]
+  obs <- rsofun::p_model_validation_vcmax25[1:5, ]
   params_fix <- list(
     kphio           = 0.04607080,
     soilm_par_a     = 2.75687824,
@@ -99,11 +99,52 @@ test_that("test Vcmax25 calibration routine p-model (BT, rmse)", {
   
   settings <- list(
     method              = "bayesiantools",
-    targets             = c("vcmax25"),
-    sitenames           = "Reichetal_Venezuela",
     metric              = rsofun::create_cost_rmse_pmodel(params_modl = params_fix,
                                                                 setup = "FULL",
                                                                 method = "BayesianTools",
+                                                                target = 'vcmax25'),
+    control = list(
+      sampler = "DEzs",
+      settings = list(
+        nrChains = 1,
+        burnin = 1,
+        iterations = 4
+      )
+    ),
+    par = list(
+      a = list(lower=0.04, upper=0.09, init=0.05),
+      b = list(lower=0.5, upper=5, init=3.5),
+      c = list(lower=2, upper=5, init=3.5)
+    )
+  )
+  
+  pars <- calib_sofun(
+    drivers = drivers,
+    obs = obs,
+    settings = settings
+  )
+  
+  # test for correctly returned values
+  expect_type(pars, "list")
+})
+
+test_that("test Vcmax25 calibration routine p-model (BT, likelihood maximization)", {
+  skip_on_cran()
+  drivers <- p_model_drivers_vcmax25[1:5, ]
+  obs <- rsofun::p_model_validation_vcmax25[1:5, ]
+  params_fix <- list(
+    kphio           = 0.04607080,
+    soilm_par_a     = 2.75687824,
+    soilm_par_b     = 1.68140444,
+    tau_acclim_tempstress = 7.35259044,
+    par_shape_tempstress  = 0.09863961
+  )
+  
+  settings <- list(
+    method              = "bayesiantools",
+    targets             = c("gpp"),
+    metric              = rsofun::create_cost_likelihood_pmodel(params_modl = params_fix,
+                                                                setup = "BRC",
                                                                 target = 'vcmax25'),
     control = list(
       sampler = "DEzs",
