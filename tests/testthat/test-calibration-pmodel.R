5eae82d283de21a3efba7caddf01173a3f71435b
Pepa Aran Paredes <pepa@pop-os.localdomain>
2023-04-12 18:04:41 +0200

    unit test for joint calibration

--- tests/testthat/test-calibration-pmodel.R
+++ tests/testthat/test-calibration-pmodel.R
@@ -142,7 +142,6 @@ test_that("test Vcmax25 calibration routine p-model (BT, likelihood maximization
   
   settings <- list(
     method              = "bayesiantools",
-    targets             = c("gpp"),
     metric              = rsofun::create_cost_likelihood_pmodel(params_modl = params_fix,
                                                                 setup = "BRC",
                                                                 target = 'vcmax25'),
@@ -155,9 +154,50 @@ test_that("test Vcmax25 calibration routine p-model (BT, likelihood maximization
       )
     ),
     par = list(
-      a = list(lower=0.04, upper=0.09, init=0.05),
-      b = list(lower=0.5, upper=5, init=3.5),
-      c = list(lower=2, upper=5, init=3.5)
+      a = list(lower=0.04, upper=0.09, init=0.05)
+    )
+  )
+  
+  pars <- calib_sofun(
+    drivers = drivers,
+    obs = obs,
+    settings = settings
+  )
+  
+  # test for correctly returned values
+  expect_type(pars, "list")
+})
+
+test_that("test joint calibration routine p-model (BT, likelihood maximization)", {
+  skip_on_cran()
+  drivers <- list(gpp = rsofun::p_model_drivers, 
+                  vcmax25 = rsofun::p_model_drivers_vcmax25[1:5, ])
+  obs <- list(gpp = rsofun::p_model_validation,
+              vcmax25 = rsofun::p_model_validation_vcmax25[1:5, ])
+  params_fix <- list(
+    kphio           = 0.04607080,
+    soilm_par_a     = 2.75687824,
+    soilm_par_b     = 1.68140444,
+    tau_acclim_tempstress = 7.35259044,
+    par_shape_tempstress  = 0.09863961
+  )
+  
+  settings <- list(
+    method              = "bayesiantools",
+    metric              = rsofun::create_cost_joint_likelihood_pmodel(
+      params_modl = params_fix,
+      setup = "BRC"
+    ),
+    control = list(
+      sampler = "DEzs",
+      settings = list(
+        nrChains = 1,
+        burnin = 1,
+        iterations = 4
+      )
+    ),
+    par = list(
+      a = list(lower=0.04, upper=0.09, init=0.05)
     )
   )
   
