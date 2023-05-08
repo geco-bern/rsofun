31d4be2a49477cfe6434989b4df7db89fa1cdba4
Pepa Aran Paredes <pepa@pop-os.localdomain>
2023-05-08 17:59:01 +0200

    remove soilm_par_b from doc

--- R/runread_pmodel_f.R
+++ R/runread_pmodel_f.R
@@ -5,8 +5,7 @@
 #' @param drivers A nested data frame with one row for each site and columns
 #'  named according to the arguments of function \code{\link{run_pmodel_f_bysite}},
 #'  namely \code{sitename, params_siml, site_info, forcing} and \code{params_soil}.
-#' @param par A named list of model parameters, including \code{kphio, soilm_par_a}
-#'  and \code{soilm_par_b}. 
+#' @param par A named list of model parameters, including \code{kphio}
 #' @param makecheck A logical specifying whether checks are performed to verify
 #'  forcings. Defaults to \code{TRUE}.
 #' @param parallel A logical specifying whether simulations are to be
