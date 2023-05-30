c8efe6d5f7225e3ea2a32ea773011bbd3631694f
Pepa Aran Paredes <pepa@pop-os.localdomain>
2023-05-30 17:22:57 +0200

    remove setup and use par for selecting calibrated params

--- R/cost_rmse_pmodel.R
+++ R/cost_rmse_pmodel.R
@@ -1,27 +1,23 @@
-#' Cost function computing RMSE for calibration of different simulation setups 
+#' Cost function computing RMSE for calibration of P-model parameters
 #' 
 #' The cost function performs a P-model run for the input drivers and parameter
 #' values, and compares the output to observations of various targets by computing
 #' the root mean squared error (RMSE).
 #' 
-#' @param par A vector of values for the parameters to be calibrated. These values 
-#' can be updated by the optimizer within the calibration routine.
+#' @param par A vector of values for the parameters to be calibrated (a subset of
+#' those described in \code{\link{runread_pmodel_f}}, in order).
 #' @param obs A nested data.frame of observations, with columns \code{'sitename'}
-#' and \code{'data'} (see \code{p_model_validation} or \code{p_model_validation_vcmax25}
+#' and \code{'data'} (see \code{\link{p_model_validation}} or \code{\link{p_model_validation_vcmax25}}
 #' to check their structure). 
 #' @param drivers A nested data.frame of driver data. See \code{\link{p_model_drivers}}
 #' for a description of the data structure.
-#' @param setup A character string (\code{'BRC'} or \code{'FULL'}) indicating which
-#' parameters are calibrated. For \code{setup = 'BRC'} only the quantum yield
-#' efficiency \code{kphio} is calibrated; for \code{setup = 'FULL'} it also includes
-#' the soil moisture stress parameter \code{soilm_par_a}
-#' for calibration. 
 #' @param targets A character vector indicating the target variables for which the
 #' optimization will be done and the RMSE computed. This string must be a column 
 #' name of the \code{data} data.frame belonging to the validation nested data.frame 
 #' (for example 'gpp').
-#' @param par_fixed A vector of model parameter values to keep fixed during the
-#' calibration. Only necessary if \code{setup = 'BRC'}.
+#' @param par_fixed A named list of model parameter values to keep fixed during the
+#' calibration. These should complement the input \code{par} such that all model
+#' parameters are passed on to \code{\link{runread_pmodel_f}}.
 #' @param target_weights A vector of weights to be used in the computation of
 #' the RMSE if using several targets. By default (\code{target_weights = NULL})
 #' the RMSE is computed separately for each target and then averaged. The provided
@@ -34,7 +30,7 @@
 #' @details To run the P-model, all model parameters must be given. The cost
 #' function uses arguments \code{par} and \code{par_fixed} such that, in the
 #' calibration routine, \code{par} can be updated by the optimizer and 
-#' \code{par_fixed} kept unchanged throughout calibration.
+#' \code{par_fixed} are kept unchanged throughout calibration.
 #'   
 #' If the validation data contains a "date" column (fluxes), the simulated target time series
 #' is compared to the observed values on those same dates (e.g. for GPP). Otherwise, 
@@ -47,40 +43,44 @@
 #' @export
 
 cost_rmse_pmodel <- function(
-    par, # ordered vector
+    par,  # ordered vector of model parameters
     obs, 
     drivers,
-    setup,
     targets,    
-    par_fixed = NULL, # only necessary if setup = BRC
+    par_fixed = NULL, # non-calibrated model parameters
     target_weights = NULL # if using several targets, how are the individual RMSE weighted? named vector
 ){
   
   # predefine variables for CRAN check compliance
   sitename <- data <- gpp_mod <- NULL
   
-  ## define parameter set based on calibration setup
-  if(setup == 'BRC'){
-    if(is.null(par_fixed)){
-      stop('Error: par_fixed = NULL. 
-            Parameter value for "soilm_par_a" must be fixed.')
-    }else{
-      params_modl <- list(
-        kphio           = par[1],
-        soilm_par_a     = par_fixed['soilm_par_a']
-      )
-    }
-  }else if(setup == 'FULL'){
-    if(length(par) < 2){
-      stop('Error: Input calibratable parameter values (par) missing, cannot run P-model')
-    }else{
-      params_modl <- list(
-        kphio           = par[1],
-        soilm_par_a     = par[2]
-      )
+  ## check input parameters
+  if( (length(par) + length(par_fixed)) != 10 ){
+    stop('Error: Input calibratable and fixed parameters (par and par_fixed)
+    do not match length of the required P-model parameters.')
+  }
+  
+  ## define parameter set based on calibrated parameters
+  calib_param_names <- c('kphio', 'kphio_par_a', 'kphio_par_b',
+                         'soilm_thetastar', 'soilm_betao',
+                         'beta_unitcostratio', 'rd_to_vcmax', 
+                         'tau_acclim', 'kc_jmax', 'rootzone_whc')
+  
+  if(!is.null(par_fixed)){
+    params_modl <- list()
+    # complete with calibrated values
+    i <- 1 # start counter
+    for(par_name in calib_param_names){
+      if(is.null(par_fixed[[par_name]])){
+        params_modl[[par_name]] <- par[i]   # use calibrated par value
+        i <- i + 1                          # counter of calibrated params
+      }else{
+        params_modl[[par_name]] <- par_fixed[[par_name]]  # use fixed par value
+      }
     }
   }else{
-    stop('Only calibration setups "BRC" and "FULL" are supported.')
+    params_modl <- as.list(par)       # all parameters calibrated
+    names(par) <- calib_param_names
   }
   
   # run the model
