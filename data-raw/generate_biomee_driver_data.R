9e96574d994c7fabe71ac67116f7d7c46587a6e1
Laura Marques <laura@pop-os.localdomain>
2024-04-26 17:58:36 +0200

    Plot cohort outputs

--- data-raw/generate_biomee_driver_data.R
+++ data-raw/generate_biomee_driver_data.R
@@ -262,6 +262,12 @@ cowplot::plot_grid(
     theme_classic()+labs(x = "Year", y = "plantC")
 )
 
+biomee_gs_leuning_output_annual_cohorts %>% group_by(PFT,year) %>%
+  summarise(npp=sum(NPP*density/10000)) %>% mutate(PFT=as.factor(PFT)) %>%
+  ggplot() +
+  geom_line(aes(x = year, y = npp,col=PFT)) +
+  theme_classic()+labs(x = "Year", y = "NPP")
+
 save(biomee_gs_leuning_output,
      file ="data/biomee_gs_leuning_output.rda",
      compress = "xz")
@@ -285,6 +291,12 @@ cowplot::plot_grid(
     geom_line(aes(x = year, y = plantC)) +
     theme_classic()+labs(x = "Year", y = "plantC")
 )
+
+biomee_p_model_output_annual_cohorts %>% group_by(PFT,year) %>%
+  summarise(npp=sum(NPP*density/10000)) %>% mutate(PFT=as.factor(PFT)) %>%
+  ggplot() +
+  geom_line(aes(x = year, y = npp,col=PFT)) +
+  theme_classic()+labs(x = "Year", y = "NPP")
 
 save(biomee_p_model_output,
      file = "data/biomee_p_model_output.rda",
