c4c729e261f4891cec202d69755921743133ff4d
Beni Stocker <benjamin.stocker@gmail.com>
2024-04-28 16:58:34 +0200

    added flag for testing: -fsanitize=address

--- src/biosphere_biomee.mod.f90
+++ src/biosphere_biomee.mod.f90
@@ -197,7 +197,7 @@ contains
       !----------------------------------------------------------------
       ! Finazlize run: deallocating memory
       !----------------------------------------------------------------
-      deallocate(vegn)
+      deallocate( vegn )
 
     end if
     
