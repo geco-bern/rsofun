06c031f20f51c265d06ea5327b51b720239d4a65
Fabian Bernhard <10245680+fabern@users.noreply.github.com>
2024-10-22 14:05:50 +0200

    test: Extend loglikelihood test suite

--- tests/testthat/test-likelihoods.R
+++ tests/testthat/test-likelihoods.R
@@ -1,7 +1,7 @@
 context("test P-model and BiomeE likelihood frameworks")
 set.seed(10)
 
-test_that("test likelihood calculations", {
+# test_that("test likelihood calculations", {
   library(BayesianTools)
   # par_cal_best <- c(
   #   kphio              = 0.09423773,
@@ -13,7 +13,7 @@ test_that("test likelihood calculations", {
   #   rd_to_vcmax        = 0.014,
   #   tau_acclim         = 30.0,
   #   kc_jmax            = 0.41,
-  #   error_gpp          = 1
+  #   err_gpp          = 1
   # )
   # par_cal_min <- c(
   #   kphio              = 0.03,
@@ -25,7 +25,7 @@ test_that("test likelihood calculations", {
   #   rd_to_vcmax        = 0.01,
   #   tau_acclim         = 7.0,
   #   kc_jmax            = 0.2,
-  #   error_gpp          = 0.01
+  #   err_gpp          = 0.01
   # )
   # par_cal_max <- c(
   #   kphio              = 0.15,
@@ -37,7 +37,7 @@ test_that("test likelihood calculations", {
   #   rd_to_vcmax        = 0.1,
   #   tau_acclim         = 60.0,
   #   kc_jmax            = 0.8,
-  #   error_gpp          = 4
+  #   err_gpp          = 4
   # )
   # prior <- createUniformPrior(lower = par_cal_min, upper = par_cal_max, best = par_cal_best)
   # test_params_pmodel <- prior$sampler(4) |> as.data.frame() |>
@@ -52,12 +52,13 @@ test_that("test likelihood calculations", {
     rd_to_vcmax        = c(0.0468210919597186, 0.0725045789754949, 0.0594035412720405, 0.0491376937157475), 
     tau_acclim         = c(53.9236626827624, 26.6468327937182, 34.3328710102942, 52.5165054232348), 
     kc_jmax            = c(0.624640251602978, 0.492042974522337, 0.23459618492052, 0.502756303641945), 
-    error_gpp          = c(3.13616614692146, 2.82713630301412, 0.282233132501133, 3.00473784686066))
+    err_gpp          = c(3.13616614692146, 2.82713630301412, 0.282233132501133, 3.00473784686066))
+  test_params_pmodel <- mutate(test_params_pmodel, err_vcmax25 = 0.5) # also add error model for vcmax25
   
   # Test rsofun::cost_likelihood_pmodel
   ll_values <- apply(test_params_pmodel, 1, function(par_v) { # par_v is a vector
     rsofun::cost_likelihood_pmodel(     # likelihood cost function from package
-      par = par_v,                      # must be named
+      par = as.list(par_v),                      # must be named
       obs = rsofun::p_model_validation, # example data from package
       drivers = rsofun::p_model_drivers,
       targets = c('gpp'))
@@ -69,9 +70,9 @@ test_that("test likelihood calculations", {
                                       -2255167.68711405,
                                       -3846.02937870932))
   # Test rsofun::cost_rmse_pmodel()
-  rmse_values <- apply(dplyr::select(test_params_pmodel,-error_gpp), 1, function(par_v) { # par_v is a vector
+  rmse_values <- apply(dplyr::select(test_params_pmodel,-err_gpp, -err_vcmax25), 1, function(par_v) { # par_v is a vector
     rsofun::cost_rmse_pmodel(
-      par = par_v,  # kphio related parameters
+      par = as.list(par_v),  # kphio related parameters
       obs = rsofun::p_model_validation,
       drivers = rsofun::p_model_drivers,
       targets = c('gpp'),
@@ -85,6 +86,35 @@ test_that("test likelihood calculations", {
                                       14.0907280919599,
                                       1.38184787783589))
   
+  # Also test vcmax25 target and multi-target loglikelihoods:
+  ll_values2 <- apply(test_params_pmodel, 1, function(par_v) { # par_v is a vector
+    rsofun::cost_likelihood_pmodel(     # likelihood cost function from package
+      par     = as.list(par_v),
+      obs     = p_model_validation_vcmax25, # example data from package 
+      drivers = p_model_drivers_vcmax25,       # example data from package
+      targets = c('vcmax25'))
+  })
+  ll_values3 <- apply(test_params_pmodel, 1, function(par_v) { # par_v is a vector
+    rsofun::cost_likelihood_pmodel(     # likelihood cost function from package
+      par     = as.list(par_v),
+      obs     = rbind(p_model_validation, p_model_validation_vcmax25), # example data from package 
+      drivers = rbind(p_model_drivers, p_model_drivers_vcmax25),       # example data from package
+      targets = c('gpp', 'vcmax25'))
+  })
+  testthat::expect_equal(ll_values3, ll_values + ll_values2) # loglikelihood of multiple targets is additive
+  testthat::expect_equal(object = ll_values3, 
+                         # expected was generated with dput(ll_values3)
+                         expected = c(-4110.8773900703,
+                                      -11149.1301175005,
+                                      -2255168.59027969,
+                                      -3846.93254413504))
+  testthat::expect_equal(object = ll_values2, 
+                         # expected was generated with dput(ll_values2)
+                         expected = c(-0.903165445893757,
+                                      -0.903165590656233,
+                                      -0.903165645611412, 
+                                      -0.90316542572855))
+  
   
   # Test rsofun::cost_likelihood_biomee()
   # parBiomeE_cal_best <- c(
@@ -92,21 +122,21 @@ test_that("test likelihood calculations", {
   #   LAI_light          = 3.5,
   #   tf_base            = 1,
   #   par_mort           = 1,
-  #   error_gpp          = 1
+  #   err_gpp          = 1
   # )
   # parBiomeE_cal_min <- c(
   #   phiRL              = 0.1,
   #   LAI_light          = 0.1,
   #   tf_base            = 0.1,
   #   par_mort           = 0.1,
-  #   error_gpp          = 0.01
+  #   err_gpp          = 0.01
   # )
   # parBiomeE_cal_max <- c(
   #   phiRL              = 7.0,
   #   LAI_light          = 7.0,
   #   tf_base            = 2.0,
   #   par_mort           = 2.0,
-  #   error_gpp          = 4
+  #   err_gpp          = 4
   # )
   # prior_BiomeE <- createUniformPrior(lower = parBiomeE_cal_min, upper = parBiomeE_cal_max, best = parBiomeE_cal_best)
   # test_params_BiomeE <- prior_BiomeE$sampler(4) |> as.data.frame() |>
@@ -116,10 +146,11 @@ test_that("test likelihood calculations", {
     LAI_light = c(4.83413460890297, 4.89137732107192, 6.25084221335128, 1.65691818702035), 
     tf_base = c(0.986252965405583, 1.52580757206306, 0.278885046485811, 0.125027264398523), 
     par_mort = c(1.64211843877565, 0.579043845250271, 1.28934027748182, 1.11228716920596), 
-    error_gpp = c(2.9679689736967, 3.70911861001514, 1.16307689385489, 0.195016647893935))
+    err_gpp = c(2.9679689736967, 3.70911861001514, 1.16307689385489, 0.195016647893935))
+  # undebug(rsofun::cost_likelihood_biomee)
   ll_values_BiomeE <- apply(test_params_BiomeE, 1, function(par_v) { # par_v is a vector
-    rsofun::cost_likelihood_biomee(     # likelihood cost function from package
-      par = par_v,                      # must be named
+    rsofun::cost_likelihood_biomee(    # likelihood cost function from package
+      par = par_v,                     # must be named
       obs = rsofun::biomee_validation, # example data from package
       drivers = rsofun::biomee_gs_leuning_drivers,
       targets = c('GPP'))
@@ -131,7 +162,7 @@ test_that("test likelihood calculations", {
                                       -1.07594190198439,
                                       -13.548057740458))
   # Test rsofun::cost_rmse_biomee()
-  rmse_values_BiomeE <- apply(dplyr::select(test_params_BiomeE, -error_gpp), 1, function(par_v) { # par_v is a vector
+  rmse_values_BiomeE <- apply(dplyr::select(test_params_BiomeE, -err_gpp), 1, function(par_v) { # par_v is a vector
     rsofun::cost_rmse_biomee(     # likelihood cost function from package
       par = par_v,                      # must be named
       obs = rsofun::biomee_validation, # example data from package
@@ -148,9 +179,43 @@ test_that("test likelihood calculations", {
   #                                                   # a normal error model correspond 
   #                                                   # to the RMSE order? 
   #                        order(ll_values_BiomeE))
+  
+  # Test multi-site BiomeE loglikelihood (NOTE: pseudo-multi-site for lack of input data)
+  ll_values_BiomeE_multisite <- apply(test_params_BiomeE, 1, function(par_v) { # par_v is a vector
+    rsofun::cost_likelihood_biomee(    # likelihood cost function from package
+      par = par_v,                     # must be named
+      obs = dplyr::bind_rows(rsofun::biomee_validation, mutate(rsofun::biomee_validation, sitename = 'CH-Lae_copy')), # example data from package
+      drivers = dplyr::bind_rows(rsofun::biomee_gs_leuning_drivers, mutate(rsofun::biomee_gs_leuning_drivers, sitename = 'CH-Lae_copy')), # example data from package
+      targets = c('GPP'))
+  })
+  testthat::expect_equal(object = ll_values_BiomeE_multisite, 2 * ll_values_BiomeE)
+  testthat::expect_equal(object = ll_values_BiomeE_multisite, 
+                         # expected was generated with dput(ll_values_BiomeE_multisite) (TODO: or actually dput(ll_values_BiomeE * 2))
+                         expected = c(-4.40099424740445,
+                                      -4.47890157512484,
+                                      -2.15188380396878,
+                                      -27.096115480916))
 })
 
 
+# TODO: also make fixed error parameters work
+rsofun::cost_likelihood_pmodel(
+  par = list(err_gpp = 0.2, err_vcmax25 = 3),
+  # arguments for the cost function
+  par_fixed = list(         # fix parameter value from previous calibration
+    kc_jmax = 0.8,
+    kphio              = 0.041,
+    kphio_par_a        = 0.0,
+    kphio_par_b        = 16,
+    soilm_thetastar    = 0.6 * 240,  # to recover paper setup with soil moisture stress
+    soilm_betao        = 0.0,
+    beta_unitcostratio = 146.0,
+    rd_to_vcmax        = 0.014,      # value from Atkin et al. 2015 for C3 herbaceous
+    tau_acclim         = 30.0
+    # err_gpp = 0.2, err_vcmax25 = 3 # TODO: make this work. I.e. remove err_XXX parameters from the check also from par_fixed.
+  ),    
+  targets = c('gpp', 'vcmax25')
+)
 
 # example from the Trotsiuk, Hartig, Forrester paper:
 ###   #' r3pg_sim <- function( par_df, ...){
