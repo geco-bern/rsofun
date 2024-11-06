2a262017f448d18e5cea88d7d7a118154ecdc475
Beni Stocker <benjamin.stocker@gmail.com>
2024-11-06 16:43:54 +0100

    removed obsolete argument

--- src/gpp_pmodel.mod.f90
+++ src/gpp_pmodel.mod.f90
@@ -49,7 +49,7 @@ contains
   !   soilwp = 1 - wscal**(-bsoil)
   ! end function
 
-  subroutine gpp( tile, tile_fluxes, co2, climate, climate_acclimation, grid, init, in_ppfd, use_phydro, use_pml)
+  subroutine gpp( tile, tile_fluxes, co2, climate, climate_acclimation, grid, init, in_ppfd, use_phydro )
     !//////////////////////////////////////////////////////////////////
     ! Wrapper function to call to P-model. 
     ! Calculates meteorological conditions with memory based on daily
@@ -70,7 +70,6 @@ contains
     logical, intent(in) :: init                              ! is true on the very first simulation day (first subroutine call of each gridcell)
     logical, intent(in) :: in_ppfd                           ! whether to use PPFD from forcing or from SPLASH output
     logical, intent(in) :: use_phydro                        ! whether to use P-Hydro for photosynthesis and transpiration
-    logical, intent(in) :: use_pml                           ! whether to use uncoupled PM formulation for canopy transpiration (whether to plug gs into PM equation. Alternatively, 1.6gsD will be used)
 
     ! local variables
     type(outtype_pmodel) :: out_pmodel              ! list of P-model output variables
