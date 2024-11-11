3a8fe05e0822b0e8113fe83855c72adeca2c0ca2
marcadella <mayeul.marcadella@unibe.ch>
2024-11-11 13:15:38 +0100

    Raise tolerance to 1e-4

--- tests/testthat/test-quantitative-validation.R
+++ tests/testthat/test-quantitative-validation.R
@@ -45,7 +45,7 @@ test_that("biomeE quantitative check (gs leuning)", {
 
   output_annual_tile <- out$data[[1]]$output_annual_tile
 
-  expect_true(all.equal(colMeans(output_annual_tile), colMeans(biomee_gs_leuning_output), tolerance = 1e-6))
+  expect_true(all.equal(colMeans(output_annual_tile), colMeans(biomee_gs_leuning_output), tolerance = 1e-4))
 
   # If this test fails it means that the output of the model is out of sync with the data in the data directory.
   # It could either mean that:
@@ -65,7 +65,7 @@ test_that("biomeE quantitative check (p-model)", {
 
   output_annual_tile <- out$data[[1]]$output_annual_tile
 
-  expect_true(all.equal(colMeans(output_annual_tile), colMeans(biomee_p_model_output), tolerance = 1e-6))
+  expect_true(all.equal(colMeans(output_annual_tile), colMeans(biomee_p_model_output), tolerance = 1e-4))
 
   # If this test fails it means that the output of the model is out of sync with the data in the data directory.
   # It could either mean that:
