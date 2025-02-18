f399e6f55a1f9bdf9b94aa264ae0a81e8c27f012
marcadella <mayeul.marcadella@unibe.ch>
2025-02-18 17:48:22 +0100

    Add missing index and add grass example to biomee drivers (values taken from https://github.com/wengensheng/BiomeESS/blob/b34cc980fd1c7908d2bbce05661c2b72345d4ef8/para_files/parameters_ORNL_test.nml#L32)

--- data-raw/generate_biomee_drivers.R
+++ data-raw/generate_biomee_drivers.R
@@ -73,81 +73,81 @@ params_tile <- tibble(
 )
 
 params_species <- tibble(
-  lifeform          = rep(1,4),
-  phenotype         = c(0,1,1,1),
-  pt                = rep(0,4),
+  lifeform          = c(0, 1, 1, 1, 1),    # 0: grass; 1 Woody
+  phenotype         = c(0, 0, 1, 1, 1),    # 0: Deciduous; 1 Evergreen
+  pt                = c(1, 0, 0, 0, 0),    # 0: C3; 1: C4
   # Root parameters
-  alpha_FR          = rep(1.2,4),
-  rho_FR            = rep(200,4),
-  root_r            = rep(2.9E-4,4),
-  root_zeta         = rep(0.29,4),
-  Kw_root           = rep(3.5e-09,4),
-  leaf_size         = rep(0.04,4),
+  alpha_FR          = rep(1.2, 5),
+  rho_FR            = rep(200, 5),
+  root_r            = rep(2.9E-4, 5),
+  root_zeta         = rep(0.29, 5),
+  Kw_root           = rep(3.5e-09, 5),
+  leaf_size         = rep(0.04, 5),  ######### Unused
   # Photosynthesis parameters
-  Vmax              = rep(35.0E-6,4),
-  Vannual           = rep(1.2,4),
-  wet_leaf_dreg     = rep(0.3,4),
-  m_cond            = rep(7.0,4),
-  alpha_phot        = rep(0.06,4),
-  gamma_L           = rep(0.02,4),
-  gamma_LN          = rep(70.5,4),
-  gamma_SW          = rep(0.08,4),
-  gamma_FR          = rep(12.0,4),
-  tc_crit           = rep(283.16,4),
-  tc_crit_on        = rep(280.16,4),
-  gdd_crit          = rep(280.0,4),
-  betaON            = rep(0,2,4),
-  betaOFF           = rep(0,1,4),
+  Vmax              = rep(35.0E-6, 5),
+  Vannual           = rep(1.2, 5),
+  wet_leaf_dreg     = rep(0.3, 5),
+  m_cond            = rep(7.0, 7.0, 7.0, 7.0, 7.0), #
+  alpha_phot        = rep(0.06, 5),
+  gamma_L           = rep(0.02, 5),
+  gamma_LN          = rep(70.5, 5),
+  gamma_SW          = c(0.02, 0.08, 0.08, 0.08, 0.08), # Wood Acambium respiration rate (kgC/m2/yr)
+  gamma_FR          = rep(12.0, 5),
+  tc_crit           = rep(283.16, 5),
+  tc_crit_on        = rep(280.16, 5),
+  gdd_crit          = rep(280.0, 5),
+  betaON            = rep(0, 5), ######### Unused
+  betaOFF           = rep(0, 5), ######### Unused
   # Allometry parameters
-  alphaHT           = rep(36,4),
-  thetaHT           = rep(0.5,4),
-  alphaCA           = rep(150,4),
-  thetaCA           = rep(1.5,4),
-  alphaBM           = rep(5200,4),
-  thetaBM           = c(2.36,2.30,2.54,2.30),
+  alphaHT           = rep(36, 5),
+  thetaHT           = rep(0.5, 5),
+  alphaCA           = rep(150, 5),
+  thetaCA           = rep(1.5, 5),
+  alphaBM           = rep(5200, 5),
+  thetaBM           = c(2.3, 2.36, 2.30, 2.54, 2.30),
   # Reproduction parameters
-  seedlingsize      = rep(0.05,4),
-  maturalage        = rep(5,4),
-  v_seed            = rep(0.1,4),
+  seedlingsize      = rep(0.05, 5),                   # initial size of seedlings (s0_plant)
+  maturalage        = c(0, 5, 5, 5, 5),               # (AgeRepro)
+  v_seed            = rep(0.1, 5),
   # Mortality parameters
-  mortrate_d_c      = rep(0.01,4),
-  mortrate_d_u      = rep(0.075,4),
+  mortrate_d_c      = c(0.02, 0.01, 0.01, 0.01, 0.01),# canopy tree mortality rate, year-1 (r0mort_c)
+  mortrate_d_u      = rep(4.0, 0.075, 0.075, 0.075, 0.075), # understory tree mortality rate, year-1 (A_sd)
   # Leaf parameters
-  LMA               = c(0.05,0.17,0.11,0.1),
-  leafLS            = rep(1,4),
-  LNbase            = rep(0.8E-3,4),
-  CNleafsupport     = rep(80,4),
-  rho_wood          = c(590,370,350,300),
-  taperfactor       = rep(0.75,4),
-  lAImax            = rep(3.5,4),
-  tauNSC            = rep(3,4),
-  fNSNmax           = rep(5,4),
-  phiCSA            = rep(0.25E-4,4),
+  LMA               = c(0.02, 0.05, 0.17, 0.11, 0.1), # Leaf mass per unit area
+  leafLS            = rep(1, 5),
+  LNbase            = c(1.2E-3, 0.8E-3, 0.8E-3, 0.8E-3, 0.8E-3), # kgN m-2 leaf, Vmax = 0.03125*LNbase
+  CNleafsupport     = rep(80, 5),
+  rho_wood          = c(150, 590, 370, 350, 300),     #
+  taperfactor       = rep(0.75, 5),
+  lAImax            = rep(3.5, 5),                    # maximum crown LAI
+  tauNSC            = rep(3, 5),
+  fNSNmax           = rep(5, 5),
+  phiCSA            = rep(0.25E-4, 5),                # ratio of Asap/Acrown
   # C/N ratios for plant pools
-  CNleaf0           = rep(25,4),
-  CNsw0             = rep(350,4),
-  CNwood0           = rep(350,4),
-  CNroot0           = rep(40,4),
-  CNseed0           = rep(20,4),
-  Nfixrate0         = rep(0,4),
-  NfixCost0         = rep(12,4),
-  internal_gap_frac = rep(0.1,4),
+  CNleaf0           = rep(25, 5),
+  CNsw0             = rep(350, 5),
+  CNwood0           = rep(350, 5),
+  CNroot0           = rep(40, 5),
+  CNseed0           = rep(20, 5),
+  Nfixrate0         = rep(0.0, 5),                    # 0.03 kgN kgRootC-1 yr-1
+  NfixCost0         = c(0, 12, 12, 12, 12),           # N fixation carbon cost: 12 gC/gN
+  internal_gap_frac = rep(0.1,5),
   # calibratable params
-  kphio             = rep(0.05,4),
-  phiRL             = rep(3.5,4),
-  LAI_light         = rep(3.5,4)
+  kphio             = rep(0.05, 5),
+  phiRL             = c(0.7, 3.5, 3.5, 3.5, 3.5),     # Root/Leaf area ratio
+  LAI_light         = rep(3.5, 5)                      # Light-limited crown LAI
 )
 
 init_cohort <- tibble(
-  init_cohort_species = c(2),    # indicates sps # 2 - Fagus sylvatica
-  init_cohort_nindivs = c(0.05), # initial individual density, individual/m2 ! 1 indiv/m2 = 10.000 indiv/ha
-  init_cohort_bl      = c(0.0),  # initial biomass of leaves, kg C/individual
-  init_cohort_br      = c(0.0),  # initial biomass of fine roots, kg C/individual
-  init_cohort_bsw     = c(0.05), # initial biomass of sapwood, kg C/individual
-  init_cohort_bHW     = c(0.0),  # initial biomass of heartwood, kg C/tree
-  init_cohort_seedC   = c(0.0),  # initial biomass of seeds, kg C/individual
-  init_cohort_nsc     = c(0.05), # initial non-structural biomass
-  lu_index            = c(0)     # index land use (LU) containing this cohort. 0 (default) means all non-urban tiles will contain a copy.
+  init_cohort_species = rep(2, 1),    # indicates sps # 2 - Fagus sylvatica
+  init_cohort_nindivs = rep(0.05, 1), # initial individual density, individual/m2 ! 1 indiv/m2 = 10.000 indiv/ha
+  init_cohort_bl      = rep(0.0, 1),  # initial biomass of leaves, kg C/individual
+  init_cohort_br      = rep(0.0, 1),  # initial biomass of fine roots, kg C/individual
+  init_cohort_bsw     = rep(0.05, 1), # initial biomass of sapwood, kg C/individual
+  init_cohort_bHW     = rep(0.0, 1),  # initial biomass of heartwood, kg C/tree
+  init_cohort_seedC   = rep(0.0, 1),  # initial biomass of seeds, kg C/individual
+  init_cohort_nsc     = rep(0.05, 1), # initial non-structural biomass
+  lu_index            = rep(0, 1)     # index land use (LU) containing this cohort. 0 (default) means all non-urban tiles will contain a copy.
 )
 
 init_soil <- tibble( #list
