d6de98851c82d85ef5742ed5ebe6b673d5005ed1
maloan <ananda.kurth@unibe.ch>
2025-03-20 16:22:33 +0100

    debugging

--- src/gpp_biomee.mod.f90
+++ src/gpp_biomee.mod.f90
@@ -264,7 +264,6 @@ contains
                                 tc             = temp_memory, &
                                 vpd            = vpd_memory, &
                                 patm           = patm_memory, &
-                                tchome         = tchome, &
                                 c4             = .false., &
                                 method_optci   = "prentice14", &
                                 method_jmaxlim = "wang17" &
