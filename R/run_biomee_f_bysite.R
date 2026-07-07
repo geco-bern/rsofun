fd92ee8e5dfc0b0f6fbd5d2a4f026f1f304e2ad6
fabern <10245680+fabern@users.noreply.github.com>
2026-07-07 18:07:05 +0200

    For dplyr 1.2.0: replace `case_match()` => `recode_values()`
    Caused by warning:
    ! `case_match()` was deprecated in dplyr 1.2.0.
    i Please use `recode_values()` instead.
    
    To avoid issues also specify min version of dplyr as "1.2.0" and by adding dplyr::

--- R/run_biomee_f_bysite.R
+++ R/run_biomee_f_bysite.R
@@ -517,32 +517,32 @@ prepare_init_lu <- function(init_lu) {
     init_lu <- init_lu %>% mutate(preset = "unmanaged")
   }
   if (!"extra_N_input" %in% names(init_lu)) {
-    init_lu <- init_lu %>% mutate("extra_N_input" = case_match(
+    init_lu <- init_lu %>% dplyr::mutate("extra_N_input" = dplyr::recode_values(
       "preset",
       "cropland" ~ 0.01,
-      .default = 0.0
+      default = 0.0
     ))
   }
   if (!"extra_turnover_rate" %in% names(init_lu)) {
-    init_lu <- init_lu %>% mutate("extra_turnover_rate" = case_match(
+    init_lu <- init_lu %>% dplyr::mutate("extra_turnover_rate" = dplyr::recode_values(
       "preset",
       "cropland" ~ 0.2,
-      .default = 0.0
+      default = 0.0
     ))
   }
   if (!"oxidized_litter_fraction" %in% names(init_lu)) {
-    init_lu <- init_lu %>% mutate("oxidized_litter_fraction" = case_match(
+    init_lu <- init_lu %>% dplyr::mutate("oxidized_litter_fraction" = dplyr::recode_values(
       "preset",
       "cropland" ~ 0.9,
       "pasture" ~ 0.4,
-      .default = 0.0
+      default = 0.0
     ))
   }
-  init_lu <- init_lu %>% mutate(
-    "vegetated" = case_match(
+  init_lu <- init_lu %>% dplyr::mutate(
+    "vegetated" = dplyr::recode_values(
       "preset",
       "urban" ~ FALSE,
-      .default = TRUE
+      default = TRUE
     )
   )
   init_lu <- init_lu %>% select(
